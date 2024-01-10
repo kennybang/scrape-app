fc88c3e1b00d70221139e7c4b3256ff9f2b271c9
Kenny Bang <kennybaaang@gmail.com>
2024-01-10 14:20:27 +0100

    Plot team standings (faulty due to team name changes)

--- app.py
+++ app.py
@@ -2,27 +2,39 @@
 from dash import html, dcc, Dash
 import plotly.express as px
 import numpy as np
-from scrape_standings import scrape_f1_driver_standings  # Import the function from your scraping script
+from scrape_standings import scrape_f1_driver_standings, scrape_f1_team_standings  # Import the function from your scraping script
 
 
 years_to_scrape = list(range(2014, 2024))
 
 # Get data from the scraping script
-standings_data = scrape_f1_standings(years_to_scrape)
+driver_standings_data = scrape_f1_driver_standings(years_to_scrape)
+team_standings_data = scrape_f1_team_standings(years_to_scrape)
 
 #Legend order
-order = np.sort(standings_data["Full Name"].unique())
+driver_order = np.sort(driver_standings_data["Full Name"].unique())
+team_order = np.sort(team_standings_data["Team Name"].unique())
 
-fig = px.line(standings_data,
+fig_driver = px.line(driver_standings_data,
                     x='Year',
                     y='Position',
                     color='Full Name',
                     title='F1 Driver Standings Over the Years',
                     markers=True,
-                    category_orders={"Full Name": order},
+                    category_orders={"Full Name": driver_order},
+                    hover_data=["Points"])
+
+fig_team = px.line(team_standings_data,
+                    x='Year',
+                    y='Position',
+                    color='Team Name',
+                    title='F1 Team Standings Over the Years',
+                    markers=True,
+                    category_orders={"Team Name": team_order},
                     hover_data=["Points"])
     
-fig.update_yaxes(autorange="reversed")
+fig_driver.update_yaxes(autorange="reversed")
+fig_team.update_yaxes(autorange="reversed")
 
 # Create Dash app
 app = Dash(__name__)
@@ -31,8 +43,13 @@ app = Dash(__name__)
 app.layout = html.Div(children=[
     html.H1("Standings Graph"),
     dcc.Graph(
-        id='standings-graph',
-        figure=fig,
+        id='driver_standings-graph',
+        figure=fig_driver,
+        style={'height': '600px'} 
+    ),
+    dcc.Graph(
+        id='team_standings-graph',
+        figure=fig_team,
         style={'height': '600px'} 
     )
 ])

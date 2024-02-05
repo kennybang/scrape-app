79a3dfe04ecff0aec35ffbb882897bf15131fa80
Kenny Bang <kennybaaang@gmail.com>
2024-02-05 23:34:06 +0100

    Removed team line chart, added team bar chart

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ driver_standings_data = scrape_f1_driver_standings(years_to_scrape)
 team_standings_data = scrape_f1_team_standings(years_to_scrape)
 
 #Dataframe for dropdown
-years = team_standings_data[['Year']].copy().Year.unique()
+years = driver_standings_data[['Year']].copy().Year.unique()
 
 #Legend order
 driver_order = np.sort(driver_standings_data["Full Name"].unique())
@@ -31,16 +31,14 @@ fig_driver = px.line(driver_standings_data,
                     category_orders={"Full Name": driver_order},
                     hover_data=["Points"])
 
-fig_team = px.line(team_standings_data,
-                    x='Year',
-                    y='Position',
-                    color='Team Name',
-                    title='F1 Team Standings Over the Years',
-                    markers=True,
-                    category_orders={"Team Name": team_order},
-                    hover_data=["Points", "Name Then"])
+team_bar_data = team_standings_data[team_standings_data['Year'] == to_year]
 
-bar_data = driver_standings_data[driver_standings_data['Year'] == 2012]
+fig_team = px.bar(team_bar_data,
+                    x='Team Name',
+                    y='Points',
+                    title='F1 Team Standings Over the Years')
+
+bar_data = driver_standings_data[driver_standings_data['Year'] == to_year]
 
 fig_bar_driver = px.bar(bar_data,
                     x='Full Name',
@@ -52,7 +50,7 @@ fig_bar_driver = px.bar(bar_data,
 print(bar_data)
 
 fig_driver.update_yaxes(autorange="reversed")
-fig_team.update_yaxes(autorange="reversed")
+#fig_team.update_yaxes(autorange="reversed")
 
 # Create Dash app
 app = Dash(__name__)
@@ -64,33 +62,33 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='driver_standings_graph',
         figure=fig_driver,
-        style={'height': '600px'} 
+        style={'height': '800px'} 
     ),
 
     dcc.RangeSlider(years.min(), years.max(), 1, marks={years.min(): str(years.min()), years.max(): str(years.max())}, value=[from_year, to_year], tooltip={"placement": "bottom", "always_visible": True}, id='driver_slider'),
 
     html.Hr(),  # Horizontal line
 
-    dcc.Graph(
-        id='team_standings_graph',
-        figure=fig_team,
-        style={'height': '600px'} 
-    ),
+    #dcc.Graph(
+    #    id='team_standings_graph',
+    #    figure=fig_team,
+    #    style={'height': '600px'} 
+    #),
 
-    dcc.RangeSlider(years.min(), years.max(), 1, marks={years.min(): str(years.min()), years.max(): str(years.max())}, value=[from_year, to_year], tooltip={"placement": "bottom", "always_visible": True}, id='team_slider'),
+    #dcc.RangeSlider(years.min(), years.max(), 1, marks={years.min(): str(years.min()), years.max(): str(years.max())}, value=[from_year, to_year], tooltip={"placement": "bottom", "always_visible": True}, id='team_slider'),
 
-    html.Hr(),
+    #html.Hr(),
 
         html.Div(children=[
         
         html.Div(children=[
             dcc.Graph(
-            id='team_standings-graph1',
+            id='team_standings_graph',
             figure=fig_team,
             style={'height': '600px', 'flex': 1} 
              ),
 
-            dcc.RangeSlider(years.min(), years.max(), 1, marks={years.min(): str(years.min()), years.max(): str(years.max())}, value=[from_year, to_year], tooltip={"placement": "bottom", "always_visible": True}, id='some_slider'),
+            dcc.Slider(years.min(), years.max(), 1, marks={years.min(): str(years.min()), years.max(): str(years.max())}, value=years.max(), tooltip={"placement": "bottom", "always_visible": True}, id="team_slider" )
  
             ],
             style={'width': '50%' }
@@ -141,27 +139,24 @@ def update_driver_fig(value):
 
     return fig
 
+
+
 @callback(
     Output('team_standings_graph', 'figure'),
     Input('team_slider', 'value'))
-def update_team_fig(value):
-    from_year, to_year = value
-    filtered_data = team_standings_data[
-        (team_standings_data['Year'] >= from_year) & (team_standings_data['Year'] <= to_year)
-    ]
+def update_driver_year_fig(value):
 
-    fig = px.line(filtered_data,
-                    x='Year',
-                    y='Position',
-                    color='Team Name',
-                    title='F1 Team Standings Over the Years',
-                    markers=True,
-                    category_orders={"Team Name": team_order},
-                    hover_data=["Points", "Name Then"])
+    year = value
 
-    fig.update_yaxes(autorange="reversed")
+    #bar_data = driver_standings_data[driver_standings_data['Year'] == year]
+    bar_data = team_standings_data[team_standings_data['Year'] == year]
 
-    fig.update_layout(transition_duration=500)
+
+    fig = px.bar(bar_data,
+                x='Team Name',
+                y='Points',
+                title='F1 Team Points for a year',
+                )
 
     return fig
 
@@ -177,7 +172,7 @@ def update_driver_year_fig(value):
     fig = px.bar(bar_data,
                 x='Full Name',
                 y='Points',
-                title='F1 Points for a year',
+                title='F1 Driver Points for a year',
                 )
 
     return fig
